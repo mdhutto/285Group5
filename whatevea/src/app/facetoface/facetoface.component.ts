5e2215007bab7b0bfae786d2c9057d5fa0f94351
SaraRoberts <sara.roberts@selu.edu>
2017-09-28 13:07:12 -0500

    Added old Components for all pages / Routed All Components

---
+++ whatevea/src/app/facetoface/facetoface.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-facetoface',
+  templateUrl: './facetoface.component.html',
+  styleUrls: ['./facetoface.component.css']
+})
+export class FacetofaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
