2927d3266f62dc1b424b72467150db4882eccb53
raptress <w0270984@selu.edu>
2017-11-21 11:21:24 -0600

    Trying again w/ this guide
    https://github.com/blowdart/AspNetAuthorizationWorkshop/tree/core2

--- RisingPhoenix/RisingPhoenix/Startup.cs
+++ RisingPhoenix/RisingPhoenix/Startup.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -25,6 +27,14 @@ namespace RisingPhoenix
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme,
+                    options =>
+                    {
+                        options.LoginPath = new PathString("/Account/Login/");
+                        options.AccessDeniedPath = new PathString("/Account/Forbidden/");
+                    });
+
             services.AddMvc();
             var connection = @"Server=(localdb)\mssqllocaldb;Database=TRPDb;Trusted_Connection=True;";
             services.AddDbContext<TRPDbContext>(options => options.UseSqlServer(connection));
@@ -33,6 +43,8 @@ namespace RisingPhoenix
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseAuthentication();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
